48d670ea5076fd8c603de385b41a33db3173f34f
Jakob Alexander <jeilinghoff@googlemail.com>
2024-02-29 18:50:23 +0100

    added Around You section, fetching geolocation api

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -29,11 +29,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
         getTopCharts: builder.query({ query: () => '/charts/world' }),
         getSongDetails: builder.query({ query: ( {songid} ) => `/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-    })
+        getArtistDetails: builder.query({query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}`}),
+    }),
     });
 
     export const {
         useGetTopChartsQuery,
         useGetSongDetailsQuery,
         useGetSongRelatedQuery,
+        useGetArtistDetailsQuery,
+        useGetSongsByCountryQuery
     } = shazamCoreApi;
